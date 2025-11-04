886fbe2ce4042f0939f47d5c09620f2238df9daa
Scarkat <119711013+Scarkat@users.noreply.github.com>
2025-11-03 20:15:38 -0600

    Grafana, Loki y otras weas

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,39 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
+from loki_logger_handler import LokiLoggerHandler
+import os, sys, logging
 
 app = FastAPI()
+
+# set logging
+logger = logging.getLogger("custom_logger")
+logging_data = os.getenv("LOG_LEVEL", "INFO").upper()
+
+if logging_data == "DEBUG":
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+# set handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logger.level)
+formatter = logging.Formatter(
+    "%(levelname)s: %(asctime)s - %(name)s - %(message)s"
+)
+console_handler.setFormatter(formatter)
+
+# Create an instance of the custom handler
+loki_handler = LokiLoggerHandler(
+    url="http://loki:3100/loki/api/v1/push",
+    labels={"application": "FastApi"},
+    label_keys={},
+    timeout=10,
+)
+
+logger.addHandler(loki_handler)
+logger.addHandler(console_handler)
+logger.info("Logger initialized")
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,6 +49,11 @@ app.add_middleware(
 client = MongoClient("mongodb://admin_user:web3@mongo:27017/")
 database = client.practica1
 collection_historial = database.historial
+saludo = "hola mundo!"
+
+def imprimir_saludo():
+    print(saludo)
+    print("Imprimiendo saludo desde main.py")
 
 @app.get("/calculadora-fast-api/sum")
 def sum_numbers(a: float, b: float):
@@ -34,6 +70,10 @@ def sum_numbers(a: float, b: float):
         "b": b,
         "date": datetime.datetime.now(tz=datetime.timezone.utc)
     }
+
+    logger.info(f"Operación suma exitoso")
+    logger.debug(f"Operación suma: a={a}, b={b}, resultado={result}")
+
     collection_historial.insert_one(document)
 
     return {"a": a, "b": b, "result": result}
